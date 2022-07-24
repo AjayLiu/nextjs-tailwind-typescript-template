75cd75e099e69f6fe19436ed75f01784cd66415a
Ajay Liu <ajayliu2928@gmail.com>
2022-07-24 15:44:53 -0700

    Fixed glitch with v4 netlify/plugin-nextjs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 module.exports = {
     images: {
         domains: ["ajayliu.com"],
-        formats: ["image/avif", "image/webp"],
-    },
-    experimental: {
-        images: {
-            unoptimized: true,
-        },
     },
 };
